8a6bb4690fc0a2f453749974cbc3902e2fad5e7d
Gavin <gleng.email@gmail.com>
2015-10-19 14:51:48 +0100

    add log and err files

--- YHomeless_downloader_v1.py
+++ YHomeless_downloader_v1.py
@@ -6,7 +6,6 @@ import pandas as pd
 import re
 import argparse
 import json
-import datetime
 
 # url = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/463076/Detailed_LA_Level_Tables_201506.xlsx"
 # output_path = "tempYHomeless.csv"
@@ -18,9 +17,6 @@ def download(url, sheet, reqFields, outPath):
     homeReq = reqFields
 
     if len(homeReq) != 1:
-        errfile.write(str(now()) + " Requested data " + str(homeReq).strip(
-            '[]') + " don't match the excel file. This code is only for extracting data from filed 'e1b1a'. Please check the file at: " + str(url) + " . End process\n")
-        logfile.write(str(now()) + ' error and end process\n')
         sys.exit("Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. This code is only for extracting data from filed 'e1b1a'. Please check the file at: " + url)
 
@@ -31,22 +27,15 @@ def download(url, sheet, reqFields, outPath):
     try:
         socket = urllib.request.urlopen(url)
     except urllib.error.HTTPError as e:
-        errfile.write(str(now()) + ' excel download HTTPError is ' + str(e.code) + ' . End process\n')
-        logfile.write(str(now()) + ' error and end process\n')
         sys.exit('excel download HTTPError = ' + str(e.code))
     except urllib.error.URLError as e:
-        errfile.write(str(now()) + ' excel download URLError is ' + str(e.args) + ' . End process\n')
-        logfile.write(str(now()) + ' error and end process\n')
         sys.exit('excel download URLError = ' + str(e.args))
     except Exception:
         print('excel file download error')
         import traceback
-        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End process\n')
-        logfile.write(str(now()) + ' error and end process\n')
         sys.exit('generic exception: ' + traceback.format_exc())
 
     # operate this excel file
-    logfile.write(str(now()) + ' excel file loading\n')
     xd = pd.ExcelFile(socket)
     df = xd.parse(sheet)
 
@@ -56,7 +45,6 @@ def download(url, sheet, reqFields, outPath):
     iYear = iYQ[:4]
     iQuarter = int(int(iYQ[4:])/3)
 
-    logfile.write(str(now()) + ' indicator checking\n')
     print('indicator checking------')
     for i in range(df.shape[0]):
         numCol = []
@@ -70,9 +58,6 @@ def download(url, sheet, reqFields, outPath):
             break
 
     if len(numCol) != len(homeReq):
-        errfile.write(str(now()) + " Requested data " + str(homeReq).strip(
-            '[]') + " don't match the excel file. Please check the file at: " + str(url) + " . End process\n")
-        logfile.write(str(now()) + ' error and end process\n')
         sys.exit("Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. Please check the file at: " + url)
 
@@ -80,7 +65,6 @@ def download(url, sheet, reqFields, outPath):
     for j in col:
         raw_data[j] = []
 
-    logfile.write(str(now()) + ' data reading\n')
     print('data reading------')
     for i in range(restartIndex, df.shape[0]):
         print('reading row ' + str(i))
@@ -96,18 +80,8 @@ def download(url, sheet, reqFields, outPath):
     print('writing to file ' + dName)
     dfw = pd.DataFrame(raw_data, columns=col)
     dfw.to_csv(dName, index=False)
-    logfile.write(str(now()) + ' has been extracted and saved as ' + str(dName) + '\n')
     print('Requested data has been extracted and saved as ' + dName)
-    logfile.write(str(now()) + ' finished\n')
     print("finished")
-
-def now():
-    return datetime.datetime.now()
-
-logfile = open("mylog.log", "w")
-logfile.write(str(now()) + ' start\n')
-
-errfile = open("myerror.err", "w")
 
 parser = argparse.ArgumentParser(description='Extract online Youth Homelessness Data Excel file Section 1 to .csv file.')
 parser.add_argument("--generateConfig", "-g", help="generate a config file called config_YHomeless.json",
@@ -125,7 +99,6 @@ if args.generateConfig:
 
     with open("config_YHomeless.json", "w") as outfile:
         json.dump(obj, outfile, indent=4)
-        logfile.write(str(now()) + ' config file generated and end\n')
         sys.exit("config file generated")
 
 if args.configFile == None:
@@ -133,7 +106,6 @@ if args.configFile == None:
 
 with open(args.configFile) as json_file:
     oConfig = json.load(json_file)
-    logfile.write(str(now()) + ' read config file\n')
     print("read config file")
 
 download(oConfig["url"], oConfig["sheet"], oConfig["reqFields"], oConfig["outPath"])
