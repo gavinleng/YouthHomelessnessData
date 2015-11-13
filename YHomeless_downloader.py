11e2a82a6db9010f332cb47bb62e5fbf802c9854
Gavin <gleng.email@gmail.com>
2015-11-13 11:09:04 +0000

    lib added

--- YHomeless_downloader.py
+++ YHomeless_downloader.py
@@ -1,13 +1,17 @@
 __author__ = 'G'
 
 import sys
-import urllib
+sys.path.append('../harvesterlib')
+
 import pandas as pd
 import re
 import argparse
 import json
-import datetime
-import hashlib
+
+import now
+import openurl
+import datasave as dsave
+
 
 # url = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/463076/Detailed_LA_Level_Tables_201506.xlsx"
 # output_path = "tempYHomeless.csv"
@@ -15,26 +19,24 @@ import hashlib
 # required_indicators = ["e1b1a"]
 
 
-def download(url, sheet, reqFields, outPath):
+def download(url, sheet, reqFields, outPath, col, keyCol, digitCheckCol, noDigitRemoveFields):
     homeReq = reqFields
 
     if len(homeReq) != 1:
-        errfile.write(str(now()) + " Requested data " + str(homeReq).strip(
+        errfile.write(str(now.now()) + " Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. This code is only for extracting data from filed 'e1b1a'. Please check the file at: " + str(
             url) + " . End progress\n")
-        logfile.write(str(now()) + ' error and end progress\n')
+        logfile.write(str(now.now()) + ' error and end progress\n')
         sys.exit("Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. This code is only for extracting data from filed 'e1b1a'. Please check the file at: " + url)
 
     dName = outPath
 
-    col = ['ecode', 'name', 'year', 'quarter', 'count', 'pkey']
-
     # open url
-    socket = openurl(url)
+    socket = openurl.openurl(url, logfile, errfile)
 
     # operate this excel file
-    logfile.write(str(now()) + ' excel file loading\n')
+    logfile.write(str(now.now()) + ' excel file loading\n')
     print('excel file loading------')
     xd = pd.ExcelFile(socket)
     df = xd.parse(sheet)
@@ -44,10 +46,10 @@ def download(url, sheet, reqFields, outPath):
     iYQ = listurl[len(listurl) - 1]
     iYQ = (iYQ.split('.'))[0]
     iYear = iYQ[:4]
-    iQuarter = int(int(iYQ[4:]) / 3)
+    iQuarter = str(int(int(iYQ[4:]) / 3))
 
     # indicator checking
-    logfile.write(str(now()) + ' indicator checking\n')
+    logfile.write(str(now.now()) + ' indicator checking\n')
     print('indicator checking------')
     for i in range(df.shape[0]):
         numCol = []
@@ -61,9 +63,9 @@ def download(url, sheet, reqFields, outPath):
             break
 
     if len(numCol) != len(homeReq):
-        errfile.write(str(now()) + " Requested data " + str(homeReq).strip(
+        errfile.write(str(now.now()) + " Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. Please check the file at: " + str(url) + " . End progress\n")
-        logfile.write(str(now()) + ' error and end progress\n')
+        logfile.write(str(now.now()) + ' error and end progress\n')
         sys.exit("Requested data " + str(homeReq).strip(
             '[]') + " don't match the excel file. Please check the file at: " + url)
 
@@ -72,7 +74,7 @@ def download(url, sheet, reqFields, outPath):
         raw_data[j] = []
 
     # data reading
-    logfile.write(str(now()) + ' data reading\n')
+    logfile.write(str(now.now()) + ' data reading\n')
     print('data reading------')
     for i in range(restartIndex, df.shape[0]):
         for k in numCol:
@@ -82,60 +84,11 @@ def download(url, sheet, reqFields, outPath):
                 raw_data[col[2]].append(iYear)
                 raw_data[col[3]].append(iQuarter)
                 raw_data[col[4]].append(df.iloc[i][k])
-    logfile.write(str(now()) + ' data reading end\n')
+    logfile.write(str(now.now()) + ' data reading end\n')
     print('data reading end------')
 
-    # create primary key by md5 for each row
-    logfile.write(str(now()) + ' create primary key\n')
-    print('create primary key------')
-    keyCol = [0, 2, 3]
-    raw_data[col[-1]] = fpkey(raw_data, col, keyCol)
-    logfile.write(str(now()) + ' create primary key end\n')
-    print('create primary key end------')
-
     # save csv file
-    logfile.write(str(now()) + ' writing to file\n')
-    print('writing to file ' + dName)
-    dfw = pd.DataFrame(raw_data, columns=col)
-    dfw.to_csv(dName, index=False)
-    logfile.write(str(now()) + ' has been extracted and saved as ' + str(dName) + '\n')
-    print('Requested data has been extracted and saved as ' + dName)
-    logfile.write(str(now()) + ' finished\n')
-    print("finished")
-
-def openurl(url):
-    try:
-        socket = urllib.request.urlopen(url)
-    except urllib.error.HTTPError as e:
-        errfile.write(str(now()) + ' file download HTTPError is ' + str(e.code) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('file download HTTPError = ' + str(e.code))
-    except urllib.error.URLError as e:
-        errfile.write(str(now()) + ' file download URLError is ' + str(e.args) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('file download URLError = ' + str(e.args))
-    except Exception:
-        print('file download error')
-        import traceback
-        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('generic exception: ' + traceback.format_exc())
-
-    return socket
-
-def fpkey(data, col, keyCol):
-    mystring = ''
-    pkey = []
-    for i in range(len(data[col[0]])):
-        for j in keyCol:
-            mystring += str(data[col[j]][i])
-        mymd5 = hashlib.md5(mystring.encode()).hexdigest()
-        pkey.append(mymd5)
-
-    return pkey
-
-def now():
-    return datetime.datetime.now()
+    dsave.save(raw_data, col, keyCol, digitCheckCol, noDigitRemoveFields, dName, logfile)
 
 
 parser = argparse.ArgumentParser(
@@ -150,17 +103,21 @@ if args.generateConfig:
         "url": "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/463076/Detailed_LA_Level_Tables_201506.xlsx",
         "outPath": "tempYHomeless.csv",
         "sheet": "Section 1",
-        "reqFields": ["e1b1a"]
+        "reqFields": ["e1b1a"],
+        "colFields": ['ecode', 'name', 'year', 'quarter', 'count'],
+        "primaryKeyCol": ['ecode', 'year', 'quarter'],#[0, 2, 3],
+        "digitCheckCol": ['count'],#[4],
+        "noDigitRemoveFields": []
     }
 
     logfile = open("log_tempYHomeless.log", "w")
-    logfile.write(str(now()) + ' start\n')
+    logfile.write(str(now.now()) + ' start\n')
 
     errfile = open("err_tempYHomeless.err", "w")
 
     with open("config_tempYHomeless.json", "w") as outfile:
         json.dump(obj, outfile, indent=4)
-        logfile.write(str(now()) + ' config file generated and end\n')
+        logfile.write(str(now.now()) + ' config file generated and end\n')
         sys.exit("config file generated")
 
 if args.configFile == None:
@@ -170,11 +127,11 @@ with open(args.configFile) as json_file:
     oConfig = json.load(json_file)
 
     logfile = open('log_' + oConfig["outPath"].split('.')[0] + '.log', "w")
-    logfile.write(str(now()) + ' start\n')
+    logfile.write(str(now.now()) + ' start\n')
 
     errfile = open('err_' + oConfig["outPath"].split('.')[0] + '.err', "w")
 
-    logfile.write(str(now()) + ' read config file\n')
+    logfile.write(str(now.now()) + ' read config file\n')
     print("read config file")
 
-download(oConfig["url"], oConfig["sheet"], oConfig["reqFields"], oConfig["outPath"])
+download(oConfig["url"], oConfig["sheet"], oConfig["reqFields"], oConfig["outPath"], oConfig["colFields"], oConfig["primaryKeyCol"], oConfig["digitCheckCol"], oConfig["noDigitRemoveFields"])
